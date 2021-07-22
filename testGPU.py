d9327bcc3c5b06ab204f50d239d7fd4fa0d2ee19
natbutter <natbutter@gmail.com>
2021-07-22 16:31:33 +1000

    Update testGPU.py

--- testGPU.py
+++ testGPU.py
@@ -1,15 +1,5 @@
 import torch
 
-print(torch.cuda.current_device())
-
-print(torch.cuda.device(0))
-
-print(torch.cuda.device_count())
-
-print(torch.cuda.get_device_name(0))
-
-print(torch.cuda.is_available())
-
 # setting device on GPU if available, else CPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
@@ -21,5 +11,11 @@ if device.type == 'cuda':
     print('Memory Usage:')
     print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
     print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
+    
+    print(torch.cuda.current_device())
+    print(torch.cuda.device(0))
+    print(torch.cuda.device_count())
+    print(torch.cuda.get_device_name(0))
+    print(torch.cuda.is_available())
 
 print("Finished")
